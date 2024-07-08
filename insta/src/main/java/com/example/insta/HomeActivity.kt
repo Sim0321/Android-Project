c257775756c4a02c8b841af476df325fa029f21b
Sim <0doogadooga@gmail.com>
2024-07-08 16:59:34 +0900

    fragment를 위한 어뎁터 완료

--- insta/src/main/java/com/example/insta/HomeActivity.kt
+++ insta/src/main/java/com/example/insta/HomeActivity.kt
@@ -2,6 +2,10 @@ package com.example.insta
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayout
 
 class HomeActivity : AppCompatActivity() {
@@ -17,3 +21,19 @@ class HomeActivity : AppCompatActivity() {
 }
 
 // fragment를 사용하기 위한 어뎁터 작성
+class HomePagerAdapter(
+    fragmentActivity : FragmentActivity,
+    val tabCount : Int
+) : FragmentStateAdapter(fragmentActivity){
+    override fun getItemCount(): Int {
+        return tabCount
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when(position){
+            0 -> return FeedFragment()
+            1 -> return PostFragment()
+            else -> return ProfileFragment()
+        }
+    }
+}
