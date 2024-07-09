6a24d4c4962b972b49927908c837c488c498b1ea
Sim <0doogadooga@gmail.com>
2024-07-09 14:15:49 +0900

    앨범에서 uri로 사진 가져와서 화면에 보여주기

--- insta/src/main/java/com/example/insta/PostFragment.kt
+++ insta/src/main/java/com/example/insta/PostFragment.kt
@@ -1,18 +1,47 @@
 package com.example.insta
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 
 class PostFragment : Fragment() {
+    var imageUri: Uri? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 //        원하는 fragment return
-        return inflater.inflate(R.layout.post_fragment, container,false)
+        return inflater.inflate(R.layout.post_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val selectedImageView = view.findViewById<ImageView>(R.id.selected_img)
+        val glide = Glide.with(activity as HomeActivity)
+
+
+        val imagePickerLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+//                사용자가 어떤 이미지를 선택했는지?
+                imageUri = it.data?.data
+                glide.load(imageUri).into(selectedImageView)
+            }
+        imagePickerLauncher.launch(
+            Intent(Intent.ACTION_PICK).apply {
+                this.type = MediaStore.Images.Media.CONTENT_TYPE
+            }
+        )
+
     }
 }
